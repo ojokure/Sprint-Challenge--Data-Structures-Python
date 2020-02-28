a9201a4bf267c245c74f55220da13ad95f4698a0
oladimeji-ojo <ojokuredim@gmail.com>
2020-02-28 14:06:52 +0000

    names

--- names/names.py
+++ names/names.py
@@ -1,8 +1,28 @@
 import time
-from time import process_time
 
 
-start_time = process_time()
+def binary_search(names, dup):
+    if len(names) == 0:
+        return -1
+    if names[0] > dup:
+        return -1
+    if names[len(names) - 1] < dup:
+        return -1
+    low = 0
+    high = len(names)-1
+    middle = int((high-low)/2 + low)
+    while high > low:
+        middle = int((high + low)/2)
+        if dup == names[middle]:
+            return middle
+        elif dup < names[middle]:
+            high = middle
+        else:
+            low = middle + 1
+    return -1
+
+
+start_time = time.process_time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -14,20 +34,26 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+
+names_1.sort()
+for names_2 in names_2:
+    if binary_search(names_1, names_2):
+        duplicates.append(names_2)
+
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
-a = 0
-while a < len(names_1):
-    # for name_2 in names_2:
-    if names_1[a] in names_2:
-        duplicates.append(names_1[a])
-    a += 1
+# a = 0
+# while a < len(names_1):
+#     # for name_2 in names_2:
+#     if names_1[a] in names_2:
+#         duplicates.append(names_1[a])
+#     a += 1
 
 
-end_time = process_time()
+end_time = time.process_time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
 
