0a20aa66a58731e4c4a7c265a67e86fd423a50cd
oladimeji-ojo <ojokuredim@gmail.com>
2020-02-28 12:37:56 +0000

    optimization

--- names/names.py
+++ names/names.py
@@ -1,6 +1,8 @@
 import time
+from time import process_time
 
-start_time = time.time()
+
+start_time = process_time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -13,14 +15,19 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+a = 0
+while a < len(names_1):
+    if names_1[a] in names_2:
+        duplicates.append(names_1[a])
+    a += 1
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+end_time = process_time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
